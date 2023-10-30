c2dfe8366f393a322736a5e95e1ef766a1345a62
Malika Karthik Iyer <malika@ubuntu-2004.eecis.udel.edu>
2023-10-30 16:47:49 -0400

    edited image3.c

--- image3.c
+++ image3.c
@@ -59,11 +59,11 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 //Returns: Nothing
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int span = srcImage->bpp*srcImage->bpp;
-#pragma omp parallel for
-    //int span=srcImage->bpp*srcImage->bpp;
-    for (int row=0;row<srcImage->height;row++){
-        for (int pix=0;pix<srcImage->width;pix++){
-            for (int bit=0;bit<srcImage->bpp;bit++){
+    int row, pix, bit;
+#pragma omp parallel for private(row, pix, bit)
+    for (row=0;row<srcImage->height;row++){
+        for (pix=0;pix<srcImage->width;pix++){
+            for (bit=0;bit<srcImage->bpp;bit++){
                 destImage->data[Index(pix,row,srcImage->width,bit,srcImage->bpp)]=getPixelValue(srcImage,pix,row,bit,algorithm);
             }
         }
